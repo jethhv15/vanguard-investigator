920d41679af3667ebacbc3749b130c93a9e0a2d1
jethhv15 <bapakeeee8@gmail.com>
2026-07-29 16:50:46 +0700

    feat(evidence): implement evidence identity v1

--- evidence/evidence.sh
+++ evidence/evidence.sh
@@ -2,14 +2,22 @@
 
 #
 # Vanguard Investigator
-# Evidence Builder API
+# Evidence API
 #
 
-CURRENT_EVIDENCE_ID=""
+EVIDENCE_ID=""
+EVIDENCE_SUBSYSTEM=""
+EVIDENCE_METRIC=""
+EVIDENCE_VALUE=""
+EVIDENCE_UNIT=""
 
 evidence_begin() {
 
-    CURRENT_EVIDENCE_ID=$(evidence_model_create)
+    EVIDENCE_ID="$(evidence_identity_next)"
+    EVIDENCE_SUBSYSTEM=""
+    EVIDENCE_METRIC=""
+    EVIDENCE_VALUE=""
+    EVIDENCE_UNIT=""
 
 }
 
@@ -18,42 +26,22 @@ evidence_set() {
     local key="$1"
     local value="$2"
 
-    evidence_model_set "$CURRENT_EVIDENCE_ID" "$key" "$value"
+    case "$key" in
+        subsystem) EVIDENCE_SUBSYSTEM="$value" ;;
+        metric) EVIDENCE_METRIC="$value" ;;
+        value) EVIDENCE_VALUE="$value" ;;
+        unit) EVIDENCE_UNIT="$value" ;;
+    esac
 
 }
 
 evidence_commit() {
 
-    return 0
-
-}
-
-evidence_count() {
-
-    evidence_model_count
-
-}
-
-evidence_subsystem() {
-
-    evidence_model_get "$1" subsystem
-
-}
-
-evidence_metric() {
-
-    evidence_model_get "$1" metric
-
-}
-
-evidence_value() {
-
-    evidence_model_get "$1" value
-
-}
-
-evidence_unit() {
-
-    evidence_model_get "$1" unit
+    evidence_model_add \
+        "$EVIDENCE_ID" \
+        "$EVIDENCE_SUBSYSTEM" \
+        "$EVIDENCE_METRIC" \
+        "$EVIDENCE_VALUE" \
+        "$EVIDENCE_UNIT"
 
 }

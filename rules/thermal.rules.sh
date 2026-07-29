8b5d0774b340a3bd5fd49601841284e6ef66a152
jethhv15 <bapakeeee8@gmail.com>
2026-07-29 17:09:50 +0700

    refactor(rules): introduce android performance knowledge base

---
+++ rules/thermal.rules.sh
@@ -0,0 +1,19 @@
+#!/system/bin/sh
+
+#
+# Vanguard Investigator
+# Thermal Knowledge Base
+#
+
+thermal_relationship_rule() {
+
+    local source="$1"
+    local target="$2"
+
+    case "${source}:${target}" in
+
+    esac
+
+    echo ""
+
+}

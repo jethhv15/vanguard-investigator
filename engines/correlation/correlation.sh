1980aa0b1fb0d018fce28b56e771c32fa86dd4d4
jethhv15 <bapakeeee8@gmail.com>
2026-07-29 16:49:00 +0700

    refactor(evidence): complete relationship registry api integration

--- engines/correlation/correlation.sh
+++ engines/correlation/correlation.sh
@@ -23,6 +23,30 @@ correlation_link() {
 
 }
 
+correlation_relationship_count() {
+
+    relationship_registry_count
+
+}
+
+correlation_relationship_source() {
+
+    relationship_registry_source "$1"
+
+}
+
+correlation_relationship_relation() {
+
+    relationship_registry_relation "$1"
+
+}
+
+correlation_relationship_target() {
+
+    relationship_registry_target "$1"
+
+}
+
 correlation_shutdown() {
 
     echo "[Correlation] Shutdown"

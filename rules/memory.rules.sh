5eb334bb63322ec291d6ab65a9bd6382d872f6f5
jethhv15 <bapakeeee8@gmail.com>
2026-07-29 17:09:24 +0700

    refactor(rules): introduce android performance knowledge base

---
+++ rules/memory.rules.sh
@@ -0,0 +1,19 @@
+#!/system/bin/sh
+
+#
+# Vanguard Investigator
+# Memory Knowledge Base
+#
+
+memory_relationship_rule() {
+
+    local source="$1"
+    local target="$2"
+
+    case "${source}:${target}" in
+
+    esac
+
+    echo ""
+
+}

429b4f4486113dd48c29e7a40a99a77daa68fdc1
jethhv15 <bapakeeee8@gmail.com>
2026-07-29 15:19:34 +0700

    feat(runtime): initialize runtime foundation

---
+++ runtime/bootstrap.sh
@@ -0,0 +1,21 @@
+#!/system/bin/sh
+
+#
+# Vanguard Investigator
+# Bootstrap
+#
+
+PROJECT_NAME="Vanguard Investigator"
+PROJECT_VERSION="0.1.0"
+
+bootstrap() {
+
+    echo "========================================="
+    echo "$PROJECT_NAME"
+    echo "Android Performance Inspection Framework"
+    echo "Version : $PROJECT_VERSION"
+    echo "========================================="
+
+}
+
+bootstrap

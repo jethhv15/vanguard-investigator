c888f203a6d72a9391a6dc3465c571b84be2cfdf
jethhv15 <bapakeeee8@gmail.com>
2026-07-29 16:10:42 +0700

    feat(sdk): implement inspector sdk contract v1

---
+++ sdk/inspector_sdk.sh
@@ -0,0 +1,38 @@
+#!/system/bin/sh
+
+#
+# Vanguard Investigator
+# Inspector SDK
+#
+
+inspector_init() {
+
+    return 0
+
+}
+
+inspector_metadata() {
+
+    return 0
+
+}
+
+inspect() {
+
+    echo "[Inspector SDK] inspect() is not implemented."
+
+    return 1
+
+}
+
+inspector_cleanup() {
+
+    return 0
+
+}
+
+inspector_shutdown() {
+
+    return 0
+
+}

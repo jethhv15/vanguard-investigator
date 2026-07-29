50bb3bb105378764d5e4fe32066e19e85f02cea8
jethhv15 <bapakeeee8@gmail.com>
2026-07-29 17:10:12 +0700

    refactor(rules): introduce android performance knowledge base

---
+++ rules/network.rules.sh
@@ -0,0 +1,19 @@
+#!/system/bin/sh
+
+#
+# Vanguard Investigator
+# Network Knowledge Base
+#
+
+network_relationship_rule() {
+
+    local source="$1"
+    local target="$2"
+
+    case "${source}:${target}" in
+
+    esac
+
+    echo ""
+
+}

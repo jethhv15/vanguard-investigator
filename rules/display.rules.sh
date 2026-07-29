0506258520fc55e0bc57507ca88dcdf871b5100b
jethhv15 <bapakeeee8@gmail.com>
2026-07-29 17:08:55 +0700

    refactor(rules): introduce android performance knowledge base

---
+++ rules/display.rules.sh
@@ -0,0 +1,19 @@
+#!/system/bin/sh
+
+#
+# Vanguard Investigator
+# Display Knowledge Base
+#
+
+display_relationship_rule() {
+
+    local source="$1"
+    local target="$2"
+
+    case "${source}:${target}" in
+
+    esac
+
+    echo ""
+
+}

3c751c20b0e05b8930234c22a3169cf0c8ae244c
jethhv15 <bapakeeee8@gmail.com>
2026-07-29 15:30:46 +0700

    feat(runtime): implement inspector manager

---
+++ runtime/inspector_manager.sh
@@ -0,0 +1,36 @@
+#!/system/bin/sh
+
+#
+# Vanguard Investigator
+# Inspector Manager
+#
+
+INSPECTOR_DIR="$(dirname "$0")/../inspectors"
+
+load_inspector() {
+
+    local inspector="$1"
+
+    local path="$INSPECTOR_DIR/$inspector/$inspector.sh"
+
+    if [ ! -f "$path" ]; then
+
+        echo "[Inspector] $inspector not found."
+
+        return 1
+
+    fi
+
+    . "$path"
+
+}
+
+run_inspector() {
+
+    local inspector="$1"
+
+    load_inspector "$inspector" || return 1
+
+    inspect
+
+}
